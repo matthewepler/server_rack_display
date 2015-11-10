de6b83798eb9726f9bbf8761f108431273fe1bae
Human Condition Global <matthew@humanconditionglobal.com>
2015-11-10 18:25:11 -0500

    cleanup

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ var init;
 
 function setup() {
 	// drawing environment setup
-	createCanvas(500, 500);
+	// createCanvas(500, 500);
 	background(0);
 	bars.push( new Bar( 0, height, 'rgba(9, 80, 255, '));
 	angle = 0;
